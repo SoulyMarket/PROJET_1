10ff850fe1c2e6c6c29714605494e46dd8872219
SoulyMarket <155013951+SoulyMarket@users.noreply.github.com>
2025-01-14 23:13:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -32,59 +32,69 @@ int main() {
                     printf("Choix invalide. Essayez encore.\n");
             }
         }
-        if (estConnecte == 1) {
-            printf("\n=== Menu Principal ===\n");
-            printf("3. Ajouter un produit\n");
-            printf("4. Afficher les produits\n");
-            printf("5. Modifier un produit\n");
-            printf("6. Supprimer un produit\n");
-            printf("7. Quitter\n");
-            printf("Choix : ");
-            scanf("%d", &choix);
-            
-            switch (choix) {
-                case 3:
-                    produits[nbr_produit] = ajouterProduit(nbr_produit + 1, utilisateurConnecte);
-                    nbr_produit++;
-                    break;
+ if (estConnecte == 1) {
+    printf("\n=== Menu Principal ===\n");
+    printf("3. Ajouter un produit\n");
+    printf("4. Afficher les produits\n");
+    printf("5. Modifier un produit\n");
+    printf("6. Supprimer un produit\n");
+    printf("7. Rechercher un produit\n");
+    printf("8. Trier les produits\n");
+    printf("9. Quitter\n");
+    printf("Choix : ");
+    scanf("%d", &choix);
+    switch (choix) {
+        case 3:
+            produits[nbr_produit] = ajouterProduit(nbr_produit + 1, utilisateurConnecte);
+            nbr_produit++;
+            break;
 
-                case 4:
-                    afficherProduitsDepuisCSV(utilisateurConnecte);
-                    break;
+        case 4:
+            afficherProduitsDepuisCSV(utilisateurConnecte);
+            break;
 
-                case 5:
-                    printf("Entrez l'index du produit a modifier (1 a %d) : ", nbr_produit);
-                    int indexModif;
-                    scanf("%d", &indexModif);
-                    if (indexModif > 0 && indexModif <= nbr_produit) {
-                        modifierProduit(produits,indexModif - 1);
-                    } else {
-                        printf("Index invalide.\n");
-                    }
-                    break;
-
-                case 6:
-                    printf("Entrez l'index du produit à supprimer (1 à %d) : ", nbr_produit);
-                    int indexSuppr;
-                    scanf("%d", &indexSuppr);
-                    if (indexSuppr > 0 && indexSuppr <= nbr_produit) {
-                        nbr_produit = supprimerProduit(produits, nbr_produit, indexSuppr - 1);
-                    } else {
-                        printf("Index invalide.\n");
-                    }
-                    break;
-
-                case 7:
-                    printf("Au revoir !\n");
-                    return 0;
-
-                default:
-                    printf("Choix invalide. Essayez encore.\n");
+        case 5:
+            printf("Entrez l'index du produit a modifier (1 a %d) : ", nbr_produit);
+            int indexModif;
+            scanf("%d", &indexModif);
+            if (indexModif > 0 && indexModif <= nbr_produit) {
+                modifierProduit(produits, indexModif - 1);
+            } else {
+                printf("Index invalide.\n");
             }
+            break;
+
+        case 6:
+            printf("Entrez l'index du produit à supprimer (1 à %d) : ", nbr_produit);
+            int indexSuppr;
+            scanf("%d", &indexSuppr);
+            if (indexSuppr > 0 && indexSuppr <= nbr_produit) {
+                nbr_produit = supprimerProduit(produits, nbr_produit, indexSuppr - 1);
+            } else {
+                printf("Index invalide.\n");
+            }
+            break;
+
+        case 7: {
+            char nomProduit[100];
+            printf("Entrez le nom du produit à rechercher : ");
+            scanf("%s", nomProduit);
+            rechercherProduit(produits, nbr_produit, nomProduit, utilisateurConnecte);
+            break;
         }
+
+        case 8:
+            trierProduits(produits, nbr_produit);
+            break;
+
+        case 9:
+            printf("Au revoir !\n");
+            return 0;
+
+        default:
+            printf("Choix invalide. Essayez encore.\n");
+
     }
 
-    return 0;
-}
-
+    }}}
 
