5e79b2a717bda52ac2d495ead044beb3795a4bdf
SoulyMarket <155013951+SoulyMarket@users.noreply.github.com>
2025-01-14 19:41:10 +0100

    Create projet.h

---
+++ projet.h
@@ -0,0 +1,37 @@
+#ifndef PROJET_H
+#define PROJET_H
+
+typedef struct User {
+    char username[20];
+    char password[20];
+} User;
+
+typedef struct Date {
+    int day;
+    int month;
+    int year;
+} Date;
+
+typedef struct Prod {
+    int id;
+    char name[50];
+    char description[256];
+    char username[25];
+    float price;
+    int quantity;
+    int alert_threshold;
+    Date d_last_enter;
+    Date d_last_exit;
+} Prod;
+
+void enregistrerUtilisateur(User utilisateur);
+int verifierUtilisateur(char username[20], char password[20]);
+User inscrire();
+int connecter(char username[20]);
+Prod ajouterProduit(int id, char username[20]);
+void afficherProduitsDepuisCSV(const char* utilisateur);
+int supprimerProduit(Prod produits[], int nbr_produit, int index);
+void modifierProduit(Prod produits[], int index);
+
+#endif // PROJET_H
+
